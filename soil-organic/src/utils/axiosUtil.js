f43d2975425528d75117b01d813e1bbd0022d6af
AbhiRaa <abhinav03031999@gmail.com>
2024-06-08 13:39:10 +1000

    Added Token Unauthorized redirect when session expires.

--- soil-organic/src/utils/axiosUtil.js
+++ soil-organic/src/utils/axiosUtil.js
@@ -9,11 +9,25 @@ const API = axios.create({
   }
 });
 
-// Add a request interceptor
+// Add a request interceptor to append the token to every request
 API.interceptors.request.use(function (config) {
   const token = localStorage.getItem('access_token');
   config.headers.Authorization =  token ? `Bearer ${token}` : '';
   return config;
 });
+
+// Add a response interceptor to handle expired or invalid tokens
+API.interceptors.response.use(
+  response => response, // Simply return the response if there is no error
+  error => {
+    if (error.response && error.response.status === 401) {
+      // Token is either expired or user is unauthorized
+      localStorage.removeItem('access_token'); // Clear the stored token
+      alert('Your session has expired. Please log in again.'); // Notify user
+      window.location.href = '/signin';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
